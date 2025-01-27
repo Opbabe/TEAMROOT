76a2050deca5df90f96fe4a514baa5980a3574ba
Sandra Le <lesandra007@gmail.com>
2025-01-27 02:39:17 -0800

    added sign in with google feature, other methods should also work
    IMPORTANT: i currently set up main activity to make an instance of login fragment bc at the time i coded this there was nothing in main. ill change this in future updates to merge with sandra's branch (?)

--- app/src/main/java/edu/sjsu/sase/android/roots/HomeFragment.java
+++ app/src/main/java/edu/sjsu/sase/android/roots/HomeFragment.java
@@ -56,9 +56,7 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
--- app/src/main/java/edu/sjsu/sase/android/roots/LoginFragment.java
+++ app/src/main/java/edu/sjsu/sase/android/roots/LoginFragment.java
@@ -1,12 +1,23 @@
 package edu.sjsu.sase.android.roots;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import com.google.android.gms.common.SignInButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,33 +26,18 @@ import android.view.ViewGroup;
  */
 public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final int RC_SIGN_IN = 9001;  // Request code for Google Sign-In
+    private GoogleSignInClient mGoogleSignInClient;
 
     public LoginFragment() {
-        // Required empty public constructor
+        //required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static LoginFragment newInstance(String param1, String param2) {
         LoginFragment fragment = new LoginFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("param1", param1);
+        args.putString("param2", param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +45,63 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        //initialize Google Sign-In options, later ill add manual password and email
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)) //this pulls from default webclient id, i put the client id in strings.xml
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        //inflate the layout for fragment
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        //find  sign-in button and set up click listener
+        SignInButton signInButton = rootView.findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(view -> signIn());
+
+        return rootView;
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //result returned from launching the sign-in
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                //success
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                handleSignInSuccess(account);
+            } catch (ApiException e) {
+                //failure err
+                handleSignInFailure(e);
+            }
+        }
+    }
+
+    private void handleSignInSuccess(GoogleSignInAccount account) {
+        // TO DO LATER: send info to firebase or make the manual password/email option, wip
+        String displayName = account.getDisplayName();
+        String email = account.getEmail();
+        Toast.makeText(getActivity(), "Signed in as: " + displayName, Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void handleSignInFailure(ApiException e) {
+        int statusCode = e.getStatusCode();
+        Toast.makeText(getActivity(), "Sign-in failed: " + statusCode, Toast.LENGTH_SHORT).show();
+        Log.e("LoginFragment", "Sign-in failed: " + e.getMessage() + " Status Code: " + statusCode);
     }
 }
