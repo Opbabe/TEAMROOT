bd6eee41a7ce3cce2567d03a40d0fdd244c88ed2
Opbabe <nicktran900@gmail.com>
2025-02-21 03:03:35 -0800

    Updated code with current changes

--- app/src/main/java/edu/sjsu/sase/android/roots/EventCreationFragment.java
+++ app/src/main/java/edu/sjsu/sase/android/roots/EventCreationFragment.java
@@ -1,11 +1,9 @@
 package edu.sjsu.sase.android.roots;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,17 +11,14 @@ import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EventCreationFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class EventCreationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    // TODO: Rename parameter arguments if needed
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+    // Parameters
     private String mParam1;
     private String mParam2;
 
@@ -31,15 +26,6 @@ public class EventCreationFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EventCreationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static EventCreationFragment newInstance(String param1, String param2) {
         EventCreationFragment fragment = new EventCreationFragment();
         Bundle args = new Bundle();
@@ -61,44 +47,12 @@ public class EventCreationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the new layout for this fragment
         View view = inflater.inflate(R.layout.fragment_event_creation, container, false);
-        Button profileBttn = view.findViewById(R.id.profileBttn2);
-        Button buddyBttn = view.findViewById(R.id.buddyBttn2);
-        Button homeButtn = view.findViewById(R.id.homeButtn2);
-        Button loginBttn = view.findViewById(R.id.loginBttn);
 
-        profileBttn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToProfile(view);
-            }
-        });
-
-        buddyBttn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToBuddySystem(view);
-            }
-        });
-
-        homeButtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goHome(view);
-            }
-        });
-
-        loginBttn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToLogin(view);
-            }
-        });
-
-        Button postEvent = view.findViewById(R.id.postBttn);
-
-        postEvent.setOnClickListener(new View.OnClickListener() {
+        // Find the new Post Event button and attach a click listener
+        Button btnPostEvent = view.findViewById(R.id.btnPostEvent);
+        btnPostEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToSingleEvent(view);
@@ -107,37 +61,10 @@ public class EventCreationFragment extends Fragment {
 
         return view;
     }
-//    TODO add nav from a "Post Event Button" to a single event page
-//    private goToSingleEvent(View view){
-//        NavController controller = Navigation.findNavController(view);
-//        controller.navigate(R.id.ac);
-//
-//    }
 
-    private void goToProfile(View view){
-        NavController controller = Navigation.findNavController(view);
-        controller.navigate(R.id.action_eventCreationFragment_to_userProfileFragment);
-    }
-
-    private void goToLogin(View view){
-        NavController controller = Navigation.findNavController(view);
-        controller.navigate(R.id.action_eventCreationFragment_to_loginFragment);
-    }
-
-    private void goToBuddySystem(View view){
-        NavController controller = Navigation.findNavController(view);
-        controller.navigate(R.id.action_eventCreationFragment_to_buddySystemFragment);
-
-    }
-
-    private void goHome(View view){
-        NavController controller = Navigation.findNavController(view);
-        controller.navigate(R.id.action_eventCreationFragment_to_homeFragment);
-
-    }
-
-    private void goToSingleEvent(View view){
+    private void goToSingleEvent(View view) {
         NavController controller = Navigation.findNavController(view);
         controller.navigate(R.id.action_eventCreationFragment_to_singleEventFragment);
     }
 }
+
--- app/src/main/java/edu/sjsu/sase/android/roots/buddy/BuddySystemFragment.java
+++ app/src/main/java/edu/sjsu/sase/android/roots/buddy/BuddySystemFragment.java
@@ -1,54 +1,36 @@
 package edu.sjsu.sase.android.roots.buddy;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
 import edu.sjsu.sase.android.roots.R;
 
-import edu.sjsu.sase.android.roots.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BuddySystemFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BuddySystemFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CardView swipeCard;
+    private float x1, x2, y1, y2;
+    private static final int MIN_DISTANCE = 150;
 
     public BuddySystemFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BuddySystemFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static BuddySystemFragment newInstance(String param1, String param2) {
         BuddySystemFragment fragment = new BuddySystemFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("param1", param1);
+        args.putString("param2", param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,43 +38,107 @@ public class BuddySystemFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // Retrieve and process any arguments if needed.
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+                             Bundle savedInstanceState){
+        // Inflate the layout for this fragment.
         View view = inflater.inflate(R.layout.fragment_buddy_system, container, false);
 
-        //buttons
-        Button bud = view.findViewById(R.id.buddiesBtn);
-        // TODO: replace with BottomNavigationView
+        // Initialize the swipe card and bottom elements.
+        swipeCard = view.findViewById(R.id.swipeCard);
+        Button buddiesBtn = view.findViewById(R.id.buddiesBtn);
         Button homeBtn = view.findViewById(R.id.homeBtn);
+        Button buddiesSystemBtn = view.findViewById(R.id.buddiesSystemBtn);
         Button profileBtn = view.findViewById(R.id.userProfileBtn);
 
-        // setOnClickListeners
-        bud.setOnClickListener(this::goToList);
-        // TODO: replace with BottomNavigationView
+        // Set onClickListeners for the buttons.
+        buddiesBtn.setOnClickListener(this::goToList);
         homeBtn.setOnClickListener(this::onClickHome);
         profileBtn.setOnClickListener(this::onClickUserProfile);
+        buddiesSystemBtn.setOnClickListener(v -> {
+            // Already in Buddy System; you can opt to show a toast message if necessary.
+        });
+
+        // Setup swipe functionality on the CardView.
+        setupSwipeCard();
 
         return view;
     }
 
-    private void goToList(View view){
+    /**
+     * Sets up a touch listener on the CardView to detect swipes.
+     */
+    private void setupSwipeCard() {
+        swipeCard.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    x1 = event.getX();
+                    y1 = event.getY();
+                    return true; // Consume this event.
+                case MotionEvent.ACTION_UP:
+                    x2 = event.getX();
+                    y2 = event.getY();
+                    float deltaX = x2 - x1;
+                    float deltaY = y2 - y1;
+
+                    if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > MIN_DISTANCE) {
+                        if (deltaX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
+                    }
+                    return true;
+            }
+            return false;
+        });
+    }
+
+    /**
+     * Handles a left swipe on the card (for example, to reject a buddy).
+     */
+    private void onSwipeLeft() {
+        // Implement your left swipe behavior here.
+        loadNextBuddy();
+    }
+
+    /**
+     * Handles a right swipe on the card (for example, to accept a buddy).
+     */
+    private void onSwipeRight() {
+        // Implement your right swipe behavior here.
+        loadNextBuddy();
+    }
+
+    /**
+     * Loads the data for the next buddy and updates the CardView.
+     */
+    private void loadNextBuddy() {
+        // Retrieve child views from the CardView.
+        TextView buddyName = swipeCard.findViewById(R.id.buddyName);
+        TextView buddyInfo = swipeCard.findViewById(R.id.buddyInfo);
+        ImageView buddyImage = swipeCard.findViewById(R.id.buddyImage);
+
+        // For demonstration, simply update texts (replace with your data-loading logic).
+        buddyName.setText("New Buddy Name");
+        buddyInfo.setText("New Buddy Information");
+        // Optionally update the image resource.
+        buddyImage.setImageResource(R.drawable.placeholder_image);
+    }
+
+    /**
+     * Navigates to the Buddy List fragment.
+     */
+    private void goToList(View view) {
         NavController controller = Navigation.findNavController(view);
         controller.navigate(R.id.action_buddySystemFragment_to_buddyListFragment);
     }
 
-    // BOTTOM NAVIGATION BUTTONS
-
     /**
-     * Navigates to Home screen.
-     * @param view
+     * Navigates to the Home fragment.
      */
     private void onClickHome(View view) {
         NavController controller = Navigation.findNavController(view);
@@ -100,8 +146,7 @@ public class BuddySystemFragment extends Fragment {
     }
 
     /**
-     * Navigates to User Profile screen.
-     * @param view
+     * Navigates to the User Profile fragment.
      */
     private void onClickUserProfile(View view) {
         NavController controller = Navigation.findNavController(view);
